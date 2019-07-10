f552d41f87fda9e5126f224268a46f8038c818fe
madhuri.mittal@gmail.com <madhuri.mittal@gmail.com>
2019-07-10 18:50:53 -0400

    interfaces added

---
+++ RaaMS/interfaces/IRuleService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RaaMS.Models;
+
+namespace RaaMS.Interfaces
+{
+    public interface IRuleService
+    {
+        Rule GetRule(int ruleid);
+        IEnumerable<Rule> GetRules();
+    }
+}
