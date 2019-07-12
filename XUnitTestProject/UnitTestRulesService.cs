c1666a8183bdecd64b0656bdd1030831f438bb84
madhuri.mittal@gmail.com <madhuri.mittal@gmail.com>
2019-07-12 08:05:29 -0400

    Unit testing project added

---
+++ XUnitTestProject/UnitTestRulesService.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class UnitTestRulesService
+    {
+        [Fact]
+        public void TestGetRules()
+        {
+
+        }
+    }
+}
