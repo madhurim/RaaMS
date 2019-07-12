15f31f4dbe457a3e2ceefd4a06263937656e9cc8
madhuri.mittal@gmail.com <madhuri.mittal@gmail.com>
2019-07-12 09:59:36 -0400

    some changes made to unit test project

--- XUnitTestProject/UnitTestRulesService.cs
+++ XUnitTestProject/UnitTestRulesService.cs
@@ -1,10 +1,24 @@
 using System;
 using Xunit;
+using RaaMS.Services;
+using RaaMS.Interfaces;
+using NSubstitute;
+using System.Data.SqlClient;
 
 namespace XUnitTestProject
 {
     public class UnitTestRulesService
     {
+        RuleService ruleService;
+        public UnitTestRulesService()
+        {
+            var databaseService = Substitute.For<IDatabaseService>();
+            ruleService = new RuleService(databaseService);
+            databaseService
+            .GetConnection();
+           
+
+        }
         [Fact]
         public void TestGetRules()
         {
